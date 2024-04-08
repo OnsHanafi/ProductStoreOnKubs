b07c613014e62cf68bedeb2332963e4503f3bfbb
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-04-08 17:45:12 +0200

    Add project files.

---
+++ KubsMvcApp/Data/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KubsMvcApp.Models;
+
+namespace KubsMvcApp.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext (DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KubsMvcApp.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ KubsMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KubsMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
